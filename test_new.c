611d5b911a01ec11c7e3f5f08873902c156f012b
ABC <alan.cooper.100@hotmail.com>
2020-05-27 13:37:41 +0100

    Create test_new.c
    ADD THIS FILE TO REPO

---
+++ test_new.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int i;
+   // printf() displays the string inside quotation
+   printf("Hello, World!\n");
+   for (i = 1; i <= 10; i++) {
+       printf("Hello, World! %i\n",i);
+   }
+   return 0;
+}
